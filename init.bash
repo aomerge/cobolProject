218a21f9e6f773f9f6ee42063e31b11648d7932e
aomerge <codenode.ortega@gmail.com>
2024-09-11 18:11:02 -0600

    Add build script for compiling Cobol code and setting up database connection

--- init.bash
+++ init.bash
@@ -1,1 +1,18 @@
-cobc -x -o principal principal.cob subprogram.o
+
+cd src
+gcc -shared -fPIC -o libdbConection.so dbConection.c -lodbc
+cd ..
+
+cp ./src/libdbConection.so ./bin/libs
+
+rm ./src/libdbConection.so
+
+cobc -x -free setup.cob -L./bin/libs -ldbConection
+
+cp ./setup ./bin
+
+rm ./setup
+cd bin
+
+LD_PRELOAD=$(find ./libs -name '*.so' | tr '\n' ':') ./setup
+
